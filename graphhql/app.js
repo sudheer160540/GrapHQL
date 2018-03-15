12b894f1063c5676d92144625bcbd3e9b6b3a48b
sudheer160540 <sudheernunna510@gmail.com>
2018-03-15 22:57:45 +0530

    pagee genearion

--- graphhql/app.js
+++ graphhql/app.js
@@ -3,8 +3,26 @@ const app=express();
 const graphqlHTTP=require('express-graphql');
 
 
-app.use('/graphqldata',graphqlHTTP({
+var { buildSchema } = require('graphql');
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
 
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+
+};
+
+app.use('/graphqldata',graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
 }));
 app.listen(4000,()=>{
 console.log('4000 port Start.....')
